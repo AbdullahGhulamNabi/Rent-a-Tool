0635683ae04d45b785b06c76b31f26e2d44d6ffe
bilalahmadtariq7 <bilalahmadtariq789@gmail.com>
2025-01-02 23:22:09 +0500

    Tools Listing
    I this commit the tool listing task has been completed.

--- Rent-a-Tool/src/App.jsx
+++ Rent-a-Tool/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './components/Home'
+import Tools from './components/Tools'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <Navbar/>
       <Home/>
+      <Tools/>
       <Footer/>
     </>
   )
