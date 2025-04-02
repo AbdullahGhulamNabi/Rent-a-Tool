dfda7a42d621ae69cbf2ba6e8e7b2768b3f56d2b
AbdullahGhulamNabi <abdullahghulamnabi18@gmail.com>
2025-04-02 21:24:31 +0500

    Update Navbar.jsx

--- Rent-a-Tool/src/components/Nav and Footer/Navbar.jsx
+++ Rent-a-Tool/src/components/Nav and Footer/Navbar.jsx
@@ -126,6 +126,10 @@ function Navbar({ isLoggedIn }) {
     navigate("/Dashboard");
   }
 
+  function HomePage() {
+    navigate("/");
+  }
+
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
       navigate(state ? "/Dashboard/Tools" : "/Tools");
@@ -158,7 +162,7 @@ function Navbar({ isLoggedIn }) {
             </button>
           </>
         ) : (
-          <button>
+          <button onClick={HomePage}>
             <img src={home} alt="Home" className="h-7 w-7 mx-2" />
           </button>
         )}
@@ -222,7 +226,7 @@ function Navbar({ isLoggedIn }) {
                 </button>
               </>
             ) : (
-              <button className="flex items-center space-x-4 w-full text-left text-gray-700">
+              <button onClick={HomePage} className="flex items-center space-x-4 w-full text-left text-gray-700">
                 <img src={home} alt="Home" className="h-7 w-7" />
                 <span>Home</span>
               </button>
